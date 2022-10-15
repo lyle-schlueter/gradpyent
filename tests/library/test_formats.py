c80097f43b188ff228f5123115b094b6bc3283d1
Lyle Schlueter <lyle.schlueter@proton.me>
2022-10-15 12:48:29 -0400

    More tests

--- tests/library/test_formats.py
+++ tests/library/test_formats.py
@@ -208,3 +208,121 @@ class TestVerifyTwoCharHex:  # pylint: disable=too-few-public-methods
 
         """
         assert formats._verify_two_char_hex(code) == expected
+
+
+class TestGetKmlFromRgb:
+    @pytest.mark.parametrize(
+        "color,opacity,expected",
+        [
+            [
+                RGB(255, 0, 0),
+                1,
+                "ff0000ff"
+            ],
+            [
+                RGB(255, 0, 0),
+                0,
+                "000000ff"
+            ],
+            [
+                RGB(0, 255, 0),
+                1,
+                "ff00ff00"
+            ],
+            [
+                RGB(0, 0, 255),
+                1,
+                "ffff0000"
+            ],
+        ],
+    )
+    def test_return(self, color: RGB, opacity: float, expected: str) -> None:
+        """Test ``_get_kml_from_rgb``.
+
+        Args:
+            color: RGB object
+            opacity: Opacity value
+            expected: The expected exception that will be raised
+
+        """
+        assert formats._get_kml_from_rgb(rgb=color, opacity=opacity) == expected
+
+
+class TestGetHtmlFromRgb:
+    @pytest.mark.parametrize(
+        "color,expected",
+        [
+            [
+                RGB(255, 0, 0),
+                "#ff0000"
+            ],
+            [
+                RGB(0, 255, 0),
+                "#00ff00"
+            ],
+            [
+                RGB(0, 0, 255),
+                "#0000ff"
+            ],
+        ],
+    )
+    def test_return(self, color: RGB, expected: str) -> None:
+        """Test ``_get_html_from_rgb``.
+
+        Args:
+            color: RGB object
+            expected: The expected exception that will be raised
+
+        """
+        assert formats._get_html_from_rgb(rgb=color) == expected
+
+
+class TestFormatColor:
+    @pytest.mark.parametrize(
+        "color,fmt,opacity,mock_func",
+        [
+            [
+                # kml
+                RGB(255, 0, 0),
+                "kml",
+                0,
+                "_get_kml_from_rgb",
+            ],
+            [
+                # html
+                RGB(255, 0, 0),
+                "html",
+                None,
+                "_get_html_from_rgb"
+            ],
+            # [
+            #     # rgb
+            #     RGB(255, 0, 0),
+            # ],
+        ],
+    )
+    def test_return(self, color: RGB, fmt: str, opacity: float, mock_func, mocker) -> None:
+        """Test ``format_color``.
+
+        Args:
+            color: RGB object
+            fmt: Requested format
+            opacity: Opacity (for KML)
+            mock_func: Name of the function to be mocked, if any
+
+        """
+        # setup
+        mock = mocker.patch(
+            f"gradpyent.library.formats.{mock_func}",
+            # return_value=verify_hex
+        )
+
+        # run
+        formats.format_color(rgb=color, fmt=fmt, opacity=opacity)
+
+        # test
+        if fmt == "kml":
+            mock.assert_called_once_with(rgb=color, opacity=opacity)
+
+        if fmt == "html":
+            mock.assert_called_once_with(rgb=color)
